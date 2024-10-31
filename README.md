b74393521a36c0bf1f690e4214dbd3808a43ae5e
Paulo Sergio <pauloserg861@gmail.com>
2024-10-31 17:10:13 -0300

    adicionando estrutura do projeto

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Vue 3 + TypeScript + Vite
 
+TEste
+
 This template should help get you started developing with Vue 3 and TypeScript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
 
 Learn more about the recommended Project Setup and IDE Support in the [Vue Docs TypeScript Guide](https://vuejs.org/guide/typescript/overview.html#project-setup).
+
+Link do modelo do projeto <a href="https://www.figma.com/design/YdxUH7TQt6kH2GSgAOGRy9/Conversor-de-Moedas-(Community)?node-id=0-1&node-type=canvas&t=D3g0EVQ4HR3KHOyt-0">Clique aqui</a>
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,153 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { Icon } from '@iconify/vue' 
+import { ref } from 'vue';
+
+const isOptionsVisible = ref(false)
+
+const showSelectOptions = () => {
+  isOptionsVisible.value = !isOptionsVisible.value
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="main-element">
+    <div class="calculator-card">
+
+      <div class="calculation-area">
+        <div class="input-value">
+          <label for="value">Valor</label>
+          <input type="text" id="value" placeholder="Aqui vai um valor"/>
+        </div>
+
+        <div class="input-value">
+          <label for="currency">Moeda</label>
+          <div class="select" @click="showSelectOptions">
+            <p>Dolar americano</p>
+            <Icon icon="iconamoon:arrow-down-2-light" style="font-size: 28px;" />
+          </div>
+          <ul class="options" v-if="isOptionsVisible">
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+            <li>Item 5</li>
+          </ul>
+        </div>
+
+        <button class="calculte-btn">Converter em reais</button>
+      </div>
+      
+      <div class="total-area">
+        <span>US$ 1 = R$ 4,86</span>
+        <h1>486,00 Reais</h1>
+      </div>
+    
+    </div>      
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.main-element {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.main-element .calculator-card {
+  border: 2px solid #1D1F4C;
+  border-radius: 20px;
+  width: 450px;
+  height: 500px;
+  overflow: hidden;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.main-element .calculator-card .calculation-area {
+  height: 70%;
+  background-color: #141534;
+  padding: 50px 65px;
+}
+
+.main-element .calculator-card .calculation-area .input-value {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+  position: relative;
+}
+
+.main-element .calculator-card .calculation-area .input-value label {
+  color: #B2B8DE;
+  font-size: 10px;
+  text-transform: uppercase;
+}
+
+.main-element .calculator-card .calculation-area .input-value .select {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.main-element .calculator-card .calculation-area .input-value .select p {
+  font-size: 14px;
+  color: #858282;
+}
+
+.main-element .calculator-card .calculation-area .input-value .options {
+  background-color: #0E0F25;
+  position: absolute;
+  top: 65px;
+  left: 3px;
+  width: 99%;
+}
+
+.main-element .calculator-card .calculation-area .input-value .options li {
+  padding: 10px 15px;
+  color: #858282;
+  cursor: pointer;
+}
+
+.main-element .calculator-card .calculation-area .input-value .options li:hover {
+  background-color: #1D1F4C;
+}
+
+.main-element .calculator-card .calculation-area .input-value .select,
+.main-element .calculator-card .calculation-area .input-value input {
+  margin-top: 10px;
+  height: 44px;
+  outline: none;
+  border-radius: 6px;
+  padding: 0 15px;
+  color: aliceblue;
+  border: 1px solid #1D1F4C;
+  background-color: #0E0F25;
+}
+
+.main-element .calculator-card .calculation-area .calculte-btn {
+  width: 100%;
+  margin-top: 20px;
+  height: 44px;
+  border: none;
+  border-radius: 6px;
+  background-color: #2F34AB;
+  color: #fff;
+  font-weight: 600;
+}
+
+.main-element .calculator-card .total-area {
+  height: 30%;
+  background-color: #1F2151;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.main-element .calculator-card .total-area span {
+  color: #858aab;
+}
+
+.main-element .calculator-card .total-area h1 {
+  margin-top: 10px;
+  font-weight: 600;
 }
 </style>
